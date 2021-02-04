83ae214ee00eb1b2782357f5a853e1938f7ee93a
tristan-gueguen <tristan.gueguen@gmail.com>
2021-02-04 15:28:15 +0100

    removed cors options

--- src/app.js
+++ src/app.js
@@ -21,12 +21,17 @@ app.use(express.json())
 
 
 if (nodeEnv === 'dev') {
-    const corsOptions = {   origin: "*",   methods:
-        "GET,HEAD,PUT,PATCH,POST,DELETE",   allowedHeaders:
-            "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Origin,Cache-Control,Content-Type,X-Token,X-Refresh-Token",   credentials: true,   preflightContinue: false,  
-        optionsSuccessStatus: 204 };
+    console.log('loading dev cors options');
+    // const corsOptions = { 
+    //     origin: "*",     
+    //     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",   
+    //     allowedHeaders: "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Origin,Cache-Control,Content-Type,X-Token,X-Refresh-Token",
+    //     credentials: "true",
+    //     preflightContinue: false,  
+    //     optionsSuccessStatus: 204 };
     
-    app.use(cors(corsOptions));
+    // app.use(cors(corsOptions));
+    app.use(cors());
 } else {
     app.use(cors())
 }
