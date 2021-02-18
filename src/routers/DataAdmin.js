9bf47798e9ddc2739df17e2e7f79193449a29c6f
tristan-gueguen <tristan.gueguen@gmail.com>
2021-02-18 18:55:08 +0100

    try catch inside get data

--- src/routers/DataAdmin.js
+++ src/routers/DataAdmin.js
@@ -15,30 +15,34 @@ router.get('/dataAdmin/:password', [verifyAdmin], async (req, res) => {
         Questionnaire.find().populate('userId')
             .exec(function (err, answers) {
                 answers.forEach(answer => {
-                    let data = {
-                        userId: answer.userId._id,
-                        parentId: answer.userId.parentId,
-                        isParent: answer.userId.isParent
+                    try {
+                        let data = {
+                            userId: answer.userId._id,
+                            parentId: answer.userId.parentId,
+                            isParent: answer.userId.isParent
+                        }
+                        let userAnswers = {}
+                        if (answer.child) {
+                            userAnswers = answer.child
+                        } else {
+                            const parentAnswers1 = answer.questionnaire1 || []
+                            const parentAnswers2 = answer.questionnaire2 || []
+                            const parentAnswers3 = answer.questionnaire3 || []
+                            const parentAnswers4 = answer.questionnaire4 || []
+                            const parentAnswers5 = answer.questionnaire5 || []
+                            const parentAnswers6 = answer.questionnaire6 || []
+                            userAnswers = Object.assign(userAnswers, parentAnswers1)
+                            userAnswers = Object.assign(userAnswers, parentAnswers2)
+                            userAnswers = Object.assign(userAnswers, parentAnswers3)
+                            userAnswers = Object.assign(userAnswers, parentAnswers4)
+                            userAnswers = Object.assign(userAnswers, parentAnswers5)
+                            userAnswers = Object.assign(userAnswers, parentAnswers6)
+                        }
+                        data = Object.assign(data, flatten(userAnswers))
+                        ret.push(data)
+                    } catch (error) {
+                        console.log('error:', error.message);
                     }
-                    let userAnswers = {}
-                    if (answer.child) {
-                        userAnswers = answer.child
-                    } else {
-                        const parentAnswers1 = answer.questionnaire1 || []
-                        const parentAnswers2 = answer.questionnaire2 || []
-                        const parentAnswers3 = answer.questionnaire3 || []
-                        const parentAnswers4 = answer.questionnaire4 || []
-                        const parentAnswers5 = answer.questionnaire5 || []
-                        const parentAnswers6 = answer.questionnaire6 || []
-                        userAnswers = Object.assign(userAnswers, parentAnswers1)
-                        userAnswers = Object.assign(userAnswers, parentAnswers2)
-                        userAnswers = Object.assign(userAnswers, parentAnswers3)
-                        userAnswers = Object.assign(userAnswers, parentAnswers4)
-                        userAnswers = Object.assign(userAnswers, parentAnswers5)
-                        userAnswers = Object.assign(userAnswers, parentAnswers6)
-                    }
-                    data = Object.assign(data, flatten(userAnswers))
-                    ret.push(data)
                 });
                 const parser = new Parser();
                 const csv = parser.parse(ret);
